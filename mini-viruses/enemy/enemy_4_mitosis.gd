29ba072cd03e38cc781b2fa04d33dd5c1dd514c3
batuhanbaglan2003 <batuhanbaglan2003@gmail.com>
2026-02-28 22:11:44 +0300

    aa

--- mini-viruses/enemy/enemy_4_mitosis.gd
+++ mini-viruses/enemy/enemy_4_mitosis.gd
@@ -1,2 +1,39 @@
-$CollisionShape2D
-	
+# DÜŞMAN 4: Mitoz Hücresi (Bölünen Düşman)
+# Görevi: Öldüğünde yok olmak yerine iki adet daha küçük/hızlı düşmana bölünmek.
+# İstatistikleri: Orta can, orta hız. Ölünce 2 tane Kamçılı Hücre doğurur.
+
+extends CharacterBody2D
+
+@export var speed: float = 70.0
+@export var max_health: float = 15.0
+
+#diğerlerinden farklı olan kod kısmı
+@export var offspring_scene: PackedScene
+
+var current_health : float
+var player: Node2D
+
+func _ready():
+	current_health = max_health
+	player = get_parent().get_node("Player")
+
+func _physics_process(delta: float):
+	if player != null:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * speed
+		move_and_slide()
+
+func take_damage(amount: float):
+	current_health -= amount
+	if current_health <= 0:
+		die()
+
+func die():
+	if offspring_scene != null:
+		for i in range(2):
+			var new_cell = offspring_scene.instantiate()
+			var random_offset = Vector2(randf_range(-15, 15), randf_range(-15, 15))
+			new_cell.global_position = global_position + random_offset
+			get_parent().call_deferred("add_child", new_cell)
+
+	queue_free()
--- mini-viruses/enemy/enemy_5_ranged_bacteria.gd
+++ mini-viruses/enemy/enemy_5_ranged_bacteria.gd
@@ -1,1 +1,54 @@
+# DÜŞMAN 5: Asit Kusan (Menzilli Düşman)
+# Görevi: Oyuncuya belli bir mesafede durup uzaktan mermi fırlatmak.
+
 extends CharacterBody2D
+
+@export var speed: float = 50.0
+@export var max_health: float = 15.0
+@export var attack_range: float = 100.0
+@export var fire_rate: float = 2.0
+
+# enemy_5_ tarafından atılacak merminin dosyası burada
+# bu aşağıdaki "Packedscene" ksımı değişmeli heralde
+@export var projectile_scene: PackedScene
+
+var current_health: float
+var player: Node2D
+var fire_cooldown= 0.5
+
+func _ready():
+	current_health=max_health
+	player = get_parent().get_node("player")
+
+func _physics_process(delta: float):
+	if player != null:
+		var distance = global_position.distance_to(player.global_position)
+		
+		if distance > attack_range:
+			var direction = global_position.direction_to(player.global_position)
+			velocity = direction * speed
+			move_and_slide()
+		else:
+			velocity = Vector2.ZERO
+			
+		fire_cooldown -= delta
+			
+		if fire_cooldown <= 0.0 and distance <= attack_range:
+			shoot()
+			fire_cooldown = fire_rate
+
+func shoot():
+	if projectile_scene != null:
+		var bullet = projectile_scene.instantiate()
+		bullet.global_position = global_position
+		var direction = global_position.direction_to(player.global_position)
+		bullet.rotation = direction.angle()
+		get_parent().call_deferred("add_child", bullet)
+
+func take_damage(amount: float):
+		current_health -= amount
+		if current_health <= 0:
+			die()
+
+func die():
+	queue_free()
